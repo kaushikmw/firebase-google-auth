7edcd6b1a08fbfa8fac89d79e762a9659b362236
kaushikmw <41112058+kaushikmw@users.noreply.github.com>
2021-01-28 16:31:20 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Header from './pages/Header';
+import { UserProvider } from './firebase/UserProvider';
+import PageRoute from './route/PageRoute';
+import Default from './pages/Default';
+import TestPage from './pages/TestPage';
+import { UserPrivilageProvider } from './firebase/UserPrivilageProvider';
+import PrivateDMSRoute from './route/PrivateDMSRoute';
+import SearchDonor from './pages/SearchDonor';
+import DonorDetails from './pages/DonorDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserProvider>
+        <UserPrivilageProvider>
+          <BrowserRouter>
+          
+            <Header />
+            
+            <Switch>
+              <PageRoute exact path ="/home" component={Home}/>
+              <PrivateDMSRoute exact path ="/test" component={TestPage}/>
+              <PrivateDMSRoute exact path ="/donorsearch" component={SearchDonor}/>
+              <PrivateDMSRoute exact path ="/donordetails/:donorId" component={DonorDetails}/>
+              {/* <Route exact path="/test" component={TestPage}/> */}
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/">
+                <Redirect to="/login"/>
+              </Route>
+              <Default notfound/>
+            </Switch>
+            
+          </BrowserRouter>
+          </UserPrivilageProvider>
+      </UserProvider>
+    </>
   );
 }
 
