c537357c4871da51b772c505eedb84443a310fa1
kaushikmw <41112058+kaushikmw@users.noreply.github.com>
2021-02-11 13:04:20 +0530

    Added .env file & updated code accordingly

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyCi8CGjq1axwk2pzg5B89R7k0xgDr-tSnc",
-    authDomain: "react-grid-dashboard-kwr2.firebaseapp.com",
-    databaseURL: "https://react-grid-dashboard-kwr2-default-rtdb.firebaseio.com",
-    projectId: "react-grid-dashboard-kwr2",
-    storageBucket: "react-grid-dashboard-kwr2.appspot.com",
-    messagingSenderId: "657283336543",
-    appId: "1:657283336543:web:c702d78e9ade9f78ea8eea"    
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(config);
